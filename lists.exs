af954c454521bc9a857356dee1c70459b5fc1485
Sam Florin <sam.florin@hotmail.com>
2020-02-13 23:35:54 +0100

    winged functions (with lists) - ok

--- lists.exs
+++ lists.exs
@@ -22,4 +22,46 @@ defmodule Lists do
         [head | tail] = list
         tail
     end
+
+    def len([]) do 0 end
+    def len(list) do
+        [head | tail] = list
+        1 + len(tail)
+    end
+
+    def sum([]) do 0 end
+    def sum(list) do 
+        [head | tail] = list
+        head + sum(tail)
+    end
+
+    def duplicate(list, acc \\ [0]) # One parameter defaults to duplicate(parameter, [0]) 
+    def duplicate([], [0]) do [] end # Empty list
+    def duplicate([], acc) do acc end # When the list is recursivelsy traversed
+    def duplicate([head | tail], [0]) do duplicate(tail, [head, head]) end # First Call (acc = [0])
+    def duplicate([head | tail], acc) do # Standard case
+        duplicate(tail, acc ++ [head, head]) 
+    end
+
+
+    # Solve add without 2 functions??
+    def exists(elem, []) do false end
+    def exists(elem, [head | tail]) do 
+        if elem == head do
+            true
+        else
+            exists(elem, tail)
+        end
+    end
+
+    def add(elem, []) do [] end
+    def add(elem, list) do 
+        if exists(elem, list) do
+            list
+        else
+            list++[elem]
+        end
+    end
+
+    
 end
