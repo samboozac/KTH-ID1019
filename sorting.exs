8261104b44626d2487d486515d0d9b70b5118005
Sam Florin <sam.florin@hotmail.com>
2020-02-16 20:49:37 +0100

    Merge-Quick sort OK

--- sorting.exs
+++ sorting.exs
@@ -33,4 +33,67 @@ end
 # Understand Concepts
     # Merge Sort
     # Quick Sort
+
+defmodule Merge do
+    # Full Merge Sort ===================================================================
+    def sort([]) do [] end 
+    def sort([h]) do [h] end
+    def sort(list) do
+        {left, right} = split(list)
+        merge(sort(left), sort(right))
+    end
+    defp merge(left,[]) do left end
+    defp merge([], right) do right end
+    defp merge([l_head | l_tail] = left, [r_head | r_tail] = right) do
+        case r_head <= l_head do
+            true -> [r_head | merge(left, r_tail)]
+            false -> [l_head | merge(l_tail, right)]
+        end
+    end
+
+    defp split(list) do split(list, [], []) end
+    defp split([], left, right) do
+        {left, right}
+    end
+    defp split([head], left, right) do # case when only 1 element is left (odd len)
+        {[head | left], right}
+    end
+    defp split([head | tail], left, right) do
+        split((tl tail), [head | left], [(hd tail) | right])
+    end
+    # Full Merge Sort ===================================================================
+
+    # Alternative merge subpart ==================================
+    def merge2([],[], acc), do: Enum.reverse acc
+    def merge2([h | t], [], acc), do: merge2(t, [], [h | acc])
+    def merge2([], [h | t], acc), do: merge2([], t, [h | acc])
+    def merge2([lh | lt], [rh | rt], acc) when lh >= rh do
+        merge2([lh | lt], rt, [rh | acc])
+    end
+    def merge2([lh | lt], [rh | rt], acc) when lh < rh do
+        merge2(lt, [rh | rt], [lh | acc])
+    end
+    def merge2(left, right), do: merge2(left, right, [])
+    # Alternative merge subpart ==================================
+end
+
+# [head | tail] = _listName(_unsort) (To increase readability...)
+defmodule Quick do
+    # Full Quick Sort =========================================================  
+    def sort([head | []]) do [head] end
+    def sort([pivot | tail]) do
+        {less, grtr} = split(pivot, tail, [], [])
+        sort(less) ++ sort(grtr)
+    end
+    defp split(pivot, [], [], grtr) do {[pivot], grtr} end 
+    defp split(pivot, [], less, []) do {less, [pivot]} end 
+    defp split(pivot, [], less, grtr) do {less, [pivot|grtr]} end 
+    defp split(pivot, [head | tail]=_unsort, less, grtr) do
+        case head <= pivot do
+            true -> split(pivot, tail, [head|less], grtr)
+            false -> split(pivot, tail, less, [head|grtr])
+        end
+    end
+    # Full Quick Sort =========================================================  
+end
     
