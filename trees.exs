a8c8640b5cbc5935cdb1d3dc4dd7c215e8444a61
Sam Florin <sam.florin@hotmail.com>
2020-02-18 19:38:10 +0100

    INTRO Trees

---
+++ trees.exs
@@ -0,0 +1,13 @@
+defmodule Tree do
+    # ========================================= THEORY =========================================
+    # Simplest form of representing a tree:
+    ### :nil                            - Empty Tree
+    ### {:leaf, value}                  - A Leaf
+    ### {:node, value, left, right}     - A node
+
+    # Smaller values to the left, Larger values to the right -->     left < right
+    # ========================================= THEORY =========================================
+
+
+    def binary() end
+end
